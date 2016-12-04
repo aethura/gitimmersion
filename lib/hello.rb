1e4e3edd5021976182820252fc146c0264fdca82
Bhavishya Shah <aethura@gmail.com>
2016-12-04 16:26:44 +0000

    Hello uses Greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,7 @@
-# Default is "World"
-# Author: Bhavishya Shah (emailoftheday@email.com)
+require 'greeter'
+
+# Default is World
 name = ARGV.first || "World"
 
-puts "Hello, #{name}!"
+greeter = Greeter.new(name)
+puts greeter.greet
